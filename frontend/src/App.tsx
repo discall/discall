7ecca3dce344f9e9b34d0a3a8d8d360f8abcdcdb
Caio Dias <89219720+cdblader@users.noreply.github.com>
2022-05-15 19:33:25 -0300

    Implementação da tela de Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header/>
+        <div style={{height:'900px',width:'100%', backgroundColor:'#091C32', justifyContent:'center', display:'flex'}}>
+          <div style={{borderRadius:'20px',marginTop:'100px', height:'340px',width:'500px', backgroundColor:'#223C5B', display:'flex', flexDirection:'column', padding:'30px'}}>
+          <h1 style={{color:'white', marginLeft:'210px'}}>Login</h1>
+          <form action="">
+            <div>
+            <div><label style={{color:'white', fontSize:'22px'}}>Email</label></div>
+            <input style={{borderRadius:'5px', height: '30px', width:'500px'}}></input>
+            </div>
+            <div>
+            <div><label style={{color:'white', fontSize:'22px'}}>Senha</label></div>
+            <input type={'password'} style={{borderRadius:'5px', height: '30px', width:'500px'}}></input>
+            <button style={{color:'white',backgroundColor:'#4A6488',borderRadius:'5px', height: '40px', width:'508px', marginTop:'20px', fontSize:'22px'}}>Entrar</button>
+            <a href="" style={{color:'white', fontSize:'16px'}}>Cadastre-se</a>
+            </div>
+
+
+          </form>
+          </div>
+
+        </div>
     </div>
   );
 }
-
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+
+function Header(){
+    return(
+        <div style={{height:'90px',width:'100%', backgroundColor:'#4A6488', justifyContent:'center', display:'flex'}}>
+            <img src={logo} style={{height:'110px'}}/>
+            <h1 style={{color:'white',fontFamily:''}}>Discall</h1>
+        </div>
+    )
+}
+export default Header;
